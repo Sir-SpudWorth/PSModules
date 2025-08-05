8ed057cd99cb679e24be4d60690a284c0f59e484
Sir-SpudWorth <79704210+Sir-SpudWorth@users.noreply.github.com>
2025-08-05 08:26:46 +0100

    Update dnsrecon.psm1

--- dnsrecon.psm1
+++ dnsrecon.psm1
@@ -51,3 +51,78 @@ function Get-IpInfo{
 }
 
 Export-ModuleMember -Function Get-DnsRecords, Get-IpInfo
+
+
+
+
+
+
+
+
+
+
+header = @{
+            "Authorization" = "Bearer $($jwt)"
+        }
+
+
+        # Auth endpoint
+        writeLog 4 "Generating Endpoint URL"
+        if ($this.FQDN -notmatch "^https://"){
+            $this.FQDN = "https://$($this.FQDN)"
+        }
+        $authEP = "$($this.FQDN)/api/auth/authenticate"
+
+        Write-Host "Connecting to $($authEP)"
+
+        # Attempt to authenticate
+        try {
+            writeLog 3 "Attempting connection to $($authEP)"
+            $authResponse = Invoke-RestMethod -Method POST -Uri $authEP -Headers $header
+
+            # Clear the header after the request to remove it from Memory
+            $header = $null
+
+            if($authResponse.tokens.access.token){
+                writeLog 3 "Authentication successful. Token acquired"
+
+                # Set the Auth Toke, Refresh Token, and Expiry Time
+                $this.AuthToken = $authResponse.tokens.access.token 
+                $this.RefreshToken = $authResponse.tokens.refresh.token
+                $this.TokenExpiry = (Get-Date).AddSeconds($authResponse.tokens.refresh.expirySeconds)
+                writeLog 3 "Token will expire at: $($this.TokenExpiry)"
+                
+                # Set the auth header to be used in subsequent API calls
+                $this.authHeader = @{
+                    "Authorization" = "Bearer $($this.AuthToken)"
+                }
+
+            } else {
+                writeLog 1 "Response recieved but no Auth Token generated: $($authResponse.Status)" 
+                
+            }
+        }
+        catch {
+             writeLog 1 "Error during authentication: $($_.Exception.Message)" 
+             if($_.ErrorDetails){
+                writeLog 1 "Error Details: $($_.ErrorDetails.Message)"
+             }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
